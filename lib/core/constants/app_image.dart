0c6c2e1281b6661f290667773a40624fa3d67df0
Amr_BahaaEldin <108246015+AmrBahaaEldin@users.noreply.github.com>
2025-02-23 21:04:24 +0200

    add_widget

--- lib/core/constants/app_image.dart
+++ lib/core/constants/app_image.dart
@@ -16,6 +16,10 @@ static const String headPhone="assets/png/headphone.png";
 static const String blueCard="assets/png/blueCard.png";
 static const String homeOffer="assets/png/home_offer.png";
 static const String homeProfile="assets/png/img_4.png";
-
+//////////////////////////////////////////////
+  static const String accountSetup="assets/png/account_setup.png";
+  static const String apple="assets/png/apple.png";
+  static const String creditCard="assets/png/creditcard.png";
+  static const String discount="assets/png/discount_1.png";
 
 }
